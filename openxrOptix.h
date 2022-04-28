f179e451e96d94fc9d4cfee36eeeda9ea0276f82
B-i-M-o <bipul.mohanto@yahoo.com>
2022-04-28 17:06:33 +0200

    Add files via upload

---
+++ openxrOptix.h
@@ -0,0 +1,36 @@
+﻿// openxrOptix.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include <stdio.h>
+#include <vector>
+//#define XR_NO_PROTOTYPES 1
+
+
+#include "openxr/openxr.h"
+//#define XR_USE_GRAPHICS_API_VULKAN 1
+#define XR_USE_GRAPHICS_API_OPENGL 1
+#define XR_USE_PLATFORM_WIN32 1
+
+
+#include <Windows.h>
+#include "openxr/openxr_platform.h"
+
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32 1
+#define GLFW_EXPOSE_NATIVE_WGL 1
+#include <GLFW/glfw3native.h>
+
+#include "GLSLShader.h"
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/quaternion.hpp>
+#include <gtc/type_ptr.hpp>
+
+// TODO: Reference additional headers your program requires here.
